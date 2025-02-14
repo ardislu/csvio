fe32f497bfd03faaa65e501b1e9f72940d38de12
Ardis Lu <ardisglu@gmail.com>
2025-02-13 23:39:37 -0800

    Use consistent name length to make it easier to compare outputs

--- benchmark/core.bench.js
+++ benchmark/core.bench.js
@@ -4,35 +4,37 @@ import { benchmark, benchmarkIterations } from './utils.js';
 import { CSVReader, CSVTransformer, CSVWriter } from '../src/index.js';
 import { createRandomCSV, createTempFile } from '../test/utils.js';
 
+const NAME_LENGTH = 15;
+
 const temp = await createTempFile();
 process.on('exit', () => unlinkSync(temp));
 
 let iter = 0;
-await benchmarkIterations('Write 1MB', 20, async () => {
+await benchmarkIterations('Write 1MB'.padEnd(NAME_LENGTH, '.'), 20, async () => {
   await createRandomCSV(1000, 100, iter++).pipeTo(new CSVWriter(temp));
 });
 
-await benchmarkIterations('Read 1MB', 20, async () => {
+await benchmarkIterations('Read 1MB'.padEnd(NAME_LENGTH, '.'), 20, async () => {
   const r = new CSVReader(temp);
   for await (const _ of r) { }
 });
 
 iter = 0
-await benchmarkIterations('Transform 1MB', 20, async () => {
+await benchmarkIterations('Transform 1MB'.padEnd(NAME_LENGTH, '.'), 20, async () => {
   const r = createRandomCSV(1000, 100, iter++).pipeThrough(new CSVTransformer(r => r.map(f => Number(f) + 1)));
   for await (const _ of r) { }
 });
 
-await benchmark('Write 100MB', async () => {
+await benchmark('Write 100MB'.padEnd(NAME_LENGTH, '.'), async () => {
   await createRandomCSV(100000, 100, 2).pipeTo(new CSVWriter(temp));
 });
 
-await benchmark('Read 100MB', async () => {
+await benchmark('Read 100MB'.padEnd(NAME_LENGTH, '.'), async () => {
   const r = new CSVReader(temp);
   for await (const _ of r) { }
 });
 
-await benchmark('Transform 100MB', async () => {
+await benchmark('Transform 100MB'.padEnd(NAME_LENGTH, '.'), async () => {
   const r = createRandomCSV(100000, 100, 2).pipeThrough(new CSVTransformer(r => r.map(f => Number(f) + 1)));
   for await (const _ of r) { }
 });
