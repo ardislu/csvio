a7027c2d2a654a0deffd1355f7503166c1446001
Ardis Lu <ardisglu@gmail.com>
2025-06-15 01:54:34 -0700

    Remove `scripts` and `devDependencies` from packed `package.json`
    These keys are used for development purposes only, so it is wasting bandwidth to ship them with the final package.

--- npm/prepack.js
+++ npm/prepack.js
@@ -1,3 +1,8 @@
+import { copyFile } from 'node:fs/promises';
 import { execSync } from 'node:child_process';
 
 execSync('npm run types');
+
+await copyFile('./package.json', './package.backup.json');
+execSync('npm pkg delete scripts');
+execSync('npm pkg delete devDependencies');
